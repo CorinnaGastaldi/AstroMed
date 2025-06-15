451bc1f416c747bd667e054bcf4e5a51e485cf0f
Riccardo Frighi <r.frighi@campus.unimib.it>
2025-06-15 16:15:21 +0200

    12/06 - Resa dinamica la popolazione dei dati della pagina map-info

---
+++ js/map-info-functions.js
@@ -0,0 +1,34 @@
+document.addEventListener('DOMContentLoaded', () => {
+    fetch('../data/departments.json')
+        .then(response => response.json())
+        .then(data => {
+            debugger
+            const params = new URLSearchParams(window.location.search);
+            const deptId = params.get('id') || 'cardiology'; // fallback per test
+            console.log('ID da URL:', deptId);
+            console.log('Tutti gli ID disponibili:', data.map(d => d.id));
+
+            const dept = data.find(d => d.id === deptId);
+            if (!dept) return;
+
+            document.getElementById('dep-name').innerText = dept.name;
+            document.getElementById('dep-info').innerText = dept.info;
+            document.getElementById('dep-hours-weekdays').innerText = dept['opening-hours'].week;
+            document.getElementById('dep-hours-weekend').innerText = dept['opening-hours'].weekend;
+            document.getElementById('dep-category').innerText = dept.category;
+            document.getElementById('dep-location').innerText = `${dept.location.floor} - ${dept.location.entrance}`;
+            document.getElementById('dep-phone').innerText = dept.phone;
+            document.getElementById('dep-description').innerText = dept.description;
+
+            const perfContainer = document.getElementById('dep-performances');
+            if (perfContainer) {
+                perfContainer.innerHTML = '';
+                dept.performances.forEach((perf, index) => {
+                    perfContainer.innerHTML += `
+                        <p class="text-body-regular text-color-dark-grey py-3">${perf}</p>
+                        ${index < dept.performances.length - 1 ? '<div class="horizontal-divider-s"></div>' : ''}
+                    `;
+                });
+            }
+        });
+});
