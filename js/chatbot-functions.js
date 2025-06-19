5c025e9f678277ac7ce703425462e7c8b1bfa81d
Riccardo Frighi <r.frighi@campus.unimib.it>
2025-06-19 22:35:02 +0200

    19/06 - Aggiornata pagina Companion e aggiunta animazione alla pagina Chatbot

---
+++ js/chatbot-functions.js
@@ -0,0 +1,32 @@
+let isAnimating = false;
+
+document.getElementById('chatbot-bar').addEventListener('click', function() {
+    if (isAnimating) return;
+
+    const emptyConversation = document.getElementById('conversation-empty');
+    const conversation = document.getElementById('conversation');
+    if (!emptyConversation.classList.contains('d-none')) {
+        emptyConversation.classList.add('d-none');
+        conversation.classList.remove('d-none');
+    }
+
+    const hiddenMessages = Array.from(document.querySelectorAll('#conversation-messages> div.d-none'));
+    const messagesContainer = document.getElementById('conversation-messages');
+
+    if (hiddenMessages.length > 0) {
+        isAnimating = true;
+
+        hiddenMessages.forEach((message, index) => {
+            setTimeout(() => {
+                message.classList.remove('d-none');
+
+                // Forziamo lo scroll alla fine del contenitore dei messaggi
+                messagesContainer.scrollTop = messagesContainer.scrollHeight;
+
+                if (index === hiddenMessages.length - 1) {
+                    isAnimating = false;
+                }
+            }, index * 1500);
+        });
+    }
+});
